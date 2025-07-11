5bc92429284e484b2d46593b4136e25c5cdb6536
Priapisman677 <expeliamm02@hotmail.com>
2025-07-11 16:25:44 -0700

    update

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,29 +4,62 @@ import app from '../src/app-setup.ts'
 
 
 
-//! I need to allow the startup to be able to test out of the box.
-
 test('Should load test.env', ()=>{
     expect(process.env.TEST_VAR).toBe('TESTING');
 })
 
 
-test('Should not fail for /signup', async() => {
-    const result = await request(app).post('/api/signup').send({ username: 'Dwati', email: 'Dwati@email.com', password: '12345678' });
+//$ Test AWS write
+test('Should not fail for /put-object-single', async() => {
+
+    const testBuffer = Buffer.from('test')
+
+    const result = await request(app).put('/api/put-object-single').attach('myFieldKey', testBuffer, 'myFile.txt');
     
-    expect(result.body).toEqual({message: 'Success', data: { username: 'Dwati', email: 'Dwati@email.com', password: '12345678' }});
+    expect(result.body).toEqual({message: 'Successfully uploaded file', success: true, data: expect.any(Object)});
+
+
 });
 
-test('Should not fail for /login', async() => {
-    const result = await request(app).post('/api/signin').send({ email: 'Dwati@email.com', password: '12345678' });
+//$ Test Multer validation for wrong field
+test('Multer should reject wrong field in /put-object-single', async() => {
+
+    const testBuffer = Buffer.from('test')
+
+    const result = await request(app).put('/api/put-object-single').attach('testWrongKey', testBuffer, 'myFile.txt');
     
-    expect(result.body).toEqual({message: 'Success', data: { email: 'Dwati@email.com', password: '12345678' }});
+    expect(result.body).toEqual({message: 'Unexpected field', success: false, error: null});
+
 });
 
 
-test('Should fail for /signup', async() => {
-    const result = await request(app).post('/api/signup').send({ username: 'abc', email: 'agugutata', password: '123' });
+// $ Test Multer validation for >10MB file
+test('Multer should reject wrong field in /put-object-single', async() => {
+
+    const testBuffer = Buffer.alloc(10 * 1024 * 1024 + 1)
+
+    const result = await request(app).put('/api/put-object-single').attach('myFieldKey', testBuffer, 'myFile.txt');
     
-    expect(result.body.message[0]).toBe("Invalid email");
-    expect(result.body.message[1]).toBe("Password must be at least 6 characters long");
+    expect(result.body).toEqual({message: 'File too large', success: false, error: null});
+
+});
+
+
+
+
+//$ Test Zod validation
+test('Zod should reject name-less file in /put-object-single', async() => {
+    const testBuffer = Buffer.from('test')
+
+    const result = await request(app).put('/api/put-object-single').attach('myFieldKey', testBuffer);
+    
+    expect(result.body).toEqual({message: ["File is required"], success: false, error: null});
+});
+
+//$ Should list files
+//! This depends on "put-object-single" test
+test('Should list files in /list-all-objects', async() => {
+    const result = await request(app).get('/api/list-all-objects');
+    
+    expect(result.body).toEqual({message: 'Successfully retrieved all objects.', success: true, data: expect.any(Array)});
 });
